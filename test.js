3c17f5820c40761b230454c53c3c9354d339de5d
Mayur Patil <mayurpatil@Mayurs-MacBook-Air.local>
2023-05-15 23:12:12 +0530

    add tests

--- test.js
+++ test.js
@@ -1,7 +1,15 @@
 
+const add = require('./helper')
+
 function runTest () {
   console.log('Running Test')
   console.log('Test Ran Successfully')
 }
 
 runTest()
+
+test('should add 2 numbers', () => {
+  add(4, 5)
+
+  expect(add(7, 6)).toBe(13)
+})
